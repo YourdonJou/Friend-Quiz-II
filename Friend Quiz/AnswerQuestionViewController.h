1cc4b3cd9fb388289bd3bb7fadfd50666ffc4ee0
YourdonJou <yourdonjou@gmail.com>
2014-02-26 16:44:08 -0800

    6

---
+++ Friend Quiz/AnswerQuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnswerQuestionViewController.h
+//  Friend Quiz
+//
+//  Created by Shi Yo Jou on 2014/2/26.
+//  Copyright (c) 2014年 Christian Retureta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnswerQuestionViewController : UIViewController
+
+@end
---
+++ Friend Quiz/AnswerQuestionViewController.m
@@ -0,0 +1,38 @@
+//
+//  AnswerQuestionViewController.m
+//  Friend Quiz
+//
+//  Created by Shi Yo Jou on 2014/2/26.
+//  Copyright (c) 2014年 Christian Retureta. All rights reserved.
+//
+
+#import "AnswerQuestionViewController.h"
+
+@interface AnswerQuestionViewController ()
+
+@end
+
+@implementation AnswerQuestionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
