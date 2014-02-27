4057f40f10f8fe178c6003ae38a357f890d08e4f
YourdonJou <yourdonjou@gmail.com>
2014-02-26 16:39:40 -0800

    5

---
+++ Friend Quiz/GameResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultViewController.h
+//  Friend Quiz
+//
+//  Created by Shi Yo Jou on 2014/2/26.
+//  Copyright (c) 2014年 Christian Retureta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultViewController : UIViewController
+
+@end
