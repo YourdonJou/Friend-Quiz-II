6e92c2268461e6ca48686b7f246659292ba831a7
bobbyloo92 <christian.retureta@gmail.com>
2014-03-21 18:50:24 -0700

    Fixed Segues
    Segues added as custom to each VC

---
+++ Friend Quiz/Help_QuestionViewController.m
@@ -0,0 +1,49 @@
+//
+//  Help_QuestionViewController.m
+//  Friend Quiz
+//
+//  Created by Christian Retureta on 2014-03-21.
+//  Copyright (c) 2014 Christian Retureta. All rights reserved.
+//
+
+#import "Help_QuestionViewController.h"
+
+@interface Help_QuestionViewController ()
+
+@end
+
+@implementation Help_QuestionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
