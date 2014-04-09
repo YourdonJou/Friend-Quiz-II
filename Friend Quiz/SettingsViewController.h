6bd5a61d5c0f7c4d857c7a5b46b7939fdb10dfd2
bobbyloo92 <christian.retureta@gmail.com>
2014-04-08 22:50:05 -0700

    Settings View Edited
    Added controls to Settings View

--- Friend Quiz/SettingsViewController.h
+++ Friend Quiz/SettingsViewController.h
@@ -10,6 +10,22 @@
 
 @interface SettingsViewController : UIViewController
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *listButton;
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImage;
 @property (strong, nonatomic) IBOutlet UIButton *barbutton;
+@property (strong, nonatomic) IBOutlet UIButton *soundButton;
+
+
+@property (strong, nonatomic) IBOutlet UIButton *blueScheme;
+@property (strong, nonatomic) IBOutlet UIButton *greenScheme;
+@property (strong, nonatomic) IBOutlet UIButton *acquaScheme;
+
+
+
+- (IBAction)soundOptionSelected:(id)sender;
+- (IBAction)blueSelected:(id)sender;
+- (IBAction)greenSelected:(id)sender;
+
+- (IBAction)acquaSelected:(id)sender;
+
 
 @end
--- Friend Quiz/SettingsViewController.m
+++ Friend Quiz/SettingsViewController.m
@@ -13,7 +13,13 @@
 
 @end
 
-@implementation SettingsViewController
+@implementation SettingsViewController{
+    
+    
+    BOOL soundOn;
+   // UIImage *backImage;
+    UIImage *soundImage;
+}
 
 @synthesize barbutton;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -28,6 +34,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    soundOn = NO;
 	// Do any additional setup after loading the view.
     
     self.listButton.target = self.revealViewController;
@@ -50,4 +58,64 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)soundOptionSelected:(id)sender {
+    
+    
+    if(!soundOn){
+        
+        
+        UIImage *soundOnImage = [UIImage imageNamed:@"sound-off.png"];
+        
+        [self.soundButton setImage:soundOnImage forState:UIControlStateNormal];
+        soundOn = YES;
+        
+        NSLog(@"Sound is not ON");
+
+
+        
+    }
+    
+    else{
+        
+         UIImage *soundOffImage  = [UIImage imageNamed:@"soundon.png"];
+        
+        [self.soundButton setImage:soundOffImage forState:UIControlStateNormal];
+        
+        soundOn = NO;
+        
+        NSLog(@"Sound is  ON");
+
+        
+    }
+    
+    
+    
+}
+
+- (IBAction)blueSelected:(id)sender {
+    
+    UIImage *backImage = [UIImage imageNamed:@"pink-blueBackground.png"];
+  //  UIImageView *backgroundImage = [[UIImage alloc]initwithImage:backImage];
+    
+    self.backgroundImage.image = backImage;
+    
+    }
+
+- (IBAction)greenSelected:(id)sender {
+    
+   UIImage *backImage = [UIImage imageNamed:@"greenBackground.png"];
+    //  UIImageView *backgroundImage = [[UIImage alloc]initwithImage:backImage];
+    
+    self.backgroundImage.image = backImage;
+
+
+
+}
+- (IBAction)acquaSelected:(id)sender {
+    
+    UIImage *backImage = [UIImage imageNamed:@"pinkBackground.png"];
+    //  UIImageView *backgroundImage = [[UIImage alloc]initwithImage:backImage];
+    
+    self.backgroundImage.image = backImage;
+}
 @end
