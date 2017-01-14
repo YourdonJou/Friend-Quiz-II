c23653d39b69ffaf8a8d7d188adc1e2fae2c86c6
YourdonJou <yourdon@Yourdons-MacBook-Pro.local>
2017-01-14 11:37:35 -0800

    add a new class test

---
+++ Friend Quiz/Test Code.swift
@@ -0,0 +1,18 @@
+//
+//  Test Code.swift
+//  Friend Quiz
+//
+//  Created by Yourdon on 2017-01-14.
+//  Copyright © 2017 Christian Retureta. All rights reserved.
+//
+
+import UIKit
+
+class Test_Code: NSObject {
+
+}
+
+class Test {
+    
+    var hello = 3;
+}
